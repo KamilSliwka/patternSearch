f206b891bef774be2b3d852d95d354869a13cf03
KamilSliwka <kamilsliwinski81@gmail.com>
2023-08-03 00:35:51 +0200

    algorithm and classes finished

---
+++ BoyerMoore.cpp
@@ -0,0 +1,105 @@
+//
+// Created by kamil on 02.08.2023.
+//
+
+#include "BoyerMoore.h"
+#define NUMBEROFCHARINASCII 256
+BoyerMoore::BoyerMoore(const string &text, const string &pattern) : text(text), pattern(pattern) {
+    int m=pattern.length();
+    Last = new int[NUMBEROFCHARINASCII];
+    tempArray = new int[m+1];
+    BMNext = new int[m+1];
+    patternPosition = -1;
+}
+
+void BoyerMoore::patternSearch() {
+    initialization();
+    goodSuffixHeuristics();
+    algorithm();
+}
+
+void BoyerMoore::initialization() {
+    for(int i=0;i<NUMBEROFCHARINASCII;i++){
+        Last[i]=-1;
+    }
+    for(int i=0;i<pattern.length()+1;i++){
+        BMNext[i]=0;
+    }
+    for(int k=0;k<pattern.length();k++){
+        Last[pattern[k]]=k;
+    }
+}
+
+void BoyerMoore::goodSuffixHeuristics(){
+    int m = pattern.length();
+    int i = m;
+    int b=m+1;//prefix-suffix location of the current suffix
+    tempArray[i]=b;
+    searchForPrefixSuffixPatternSuffixes();
+    longestPrefixSuffix();
+}
+
+void BoyerMoore::searchForPrefixSuffixPatternSuffixes() {
+    int i=pattern.length();
+    int b=pattern.length()+1;
+    while(i>0){
+        while((b<=pattern.length())&&(pattern[i-1]!=pattern[b-1])){
+            if(BMNext[b]==0){
+                BMNext[b]=b-i;
+            }
+            b=tempArray[b];
+        }
+        b=b-1;
+        i=i-1;
+        tempArray[i]=b;
+    }
+}
+
+void BoyerMoore::longestPrefixSuffix(){
+    int  b=tempArray[0];
+    for(int j=0;j<=pattern.length();j++){
+        if(BMNext[j]==0){
+            BMNext[j]=b;
+        }
+        if(j==b){
+            b=tempArray[b];
+        }
+    }
+}
+
+void BoyerMoore::algorithm(){
+    int i=0;
+    int j;
+    while(i<=text.length()-pattern.length()){
+        j=pattern.length()-1;
+        while((j>-1)&&(pattern[j]==text[i+j])){
+            j--;
+        }
+        if(j==-1){
+            positionOfPattern(i);
+            i=i+BMNext[0];
+            //i++;
+        }
+        else {
+            i = i + optimalOffset(i,j);
+        }
+    }
+   isPattern();
+}
+
+void BoyerMoore::positionOfPattern(int i){
+    cout<<"index of the beginning of the pattern: "<<endl;
+    patternPosition=i;
+    cout << patternPosition << endl;
+
+}
+
+int BoyerMoore::optimalOffset(int i,int j){
+    return max(BMNext[j + 1], j - Last[text[i + j]]);
+}
+
+void BoyerMoore::isPattern(){
+    if(patternPosition == -1){
+        cout<<"search pattern is missing"<<endl;
+    }
+}
---
+++ BoyerMoore.h
@@ -0,0 +1,42 @@
+//
+// Created by kamil on 02.08.2023.
+//
+
+#ifndef PATTERNSEARCH_BOYERMOORE_H
+#define PATTERNSEARCH_BOYERMOORE_H
+#include "iostream"
+
+using namespace std;
+
+class BoyerMoore {
+private:
+    string text;
+    string pattern;
+    int* Last;
+    int* tempArray;
+    int* BMNext;
+    int patternPosition;
+public:
+    BoyerMoore(const string &text, const string &pattern);
+
+    void initialization();
+
+    void goodSuffixHeuristics();
+
+    void searchForPrefixSuffixPatternSuffixes();
+
+    void longestPrefixSuffix();
+
+    void patternSearch();
+
+    void isPattern();
+
+    void positionOfPattern(int i);
+
+    int optimalOffset(int i, int j);
+
+    void algorithm();
+};
+
+
+#endif //PATTERNSEARCH_BOYERMOORE_H
